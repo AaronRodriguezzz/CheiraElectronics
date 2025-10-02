c6ea620349170bc65aecf840cdfcb809f31546ef
AaronRodriguezzz <francisaaronrodriguez23@gmail.com>
2025-10-02 13:35:35 +0800

    Update UserLayout.jsx

--- frontend/src/layouts/UserLayout.jsx
+++ frontend/src/layouts/UserLayout.jsx
@@ -8,7 +8,6 @@ import HyperSpeedBg from "../components/user/HyperSpeedBg";
 import { useUserProtection } from '../hooks/protectHooks';
 
 export default function UserLayout() {
-  useUserProtection();
   const [profileSidebarOpen, setProfileSidebarOpen] = useState(false);
   
   return (
